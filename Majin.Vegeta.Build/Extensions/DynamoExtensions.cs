32b6d71b013c3d81c1413cf66e552d630d47b0b0
Christos Charalampidis <Chrigeta@hotmail.com>
2019-08-28 18:14:04 +0300

    Majin Vegeta initial commit

---
+++ Majin.Vegeta.Build/Extensions/DynamoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Majin.Vegeta.Build.Extensions
+{
+    /// <summary>
+    /// ExpandoObject extension methods.
+    /// </summary>
+    public static class DynamoExtensions
+    {
+        /// <summary>
+        /// Add Property to Dynamic Object or updates existing value in case of existence
+        /// </summary>
+        /// <param name="expandoDict">IDictionary</param>
+        /// <param name="propertyName">property name to Add</param>
+        /// <param name="propertyValue">property value</param>
+        public static void AddProperty(this IDictionary<string, object> expandoDict, string propertyName, object propertyValue)
+        {
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+    }
+}
---
+++ Tests/MS.Open.Api.ParserTests/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using System;
+using System.Dynamic;
+using System.IO;
+using Majin.Vegeta.Build.Extensions;
+using System.Text;
+using System.Globalization;
+
+namespace MS.Open.Api.ParserTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeserializeSwaggerDoc();
+        }
+
+        /// <summary>
+        /// Deserialize a Swagger Doc for the Fun of IT!
+        /// </summary>
+        private static void DeserializeSwaggerDoc()
+        {
+            FileStream stream = File.OpenRead("../../../../../swagger.json");
+            OpenApiDocument openApiDocument = new OpenApiStreamReader().Read(stream, out OpenApiDiagnostic diagnostic);
+
+            foreach (var pathInfo in openApiDocument.Paths.Keys)
+            {
+                Console.WriteLine($"For path {pathInfo} the supported operations are:");
+                OpenApiPathItem openPathInfo = openApiDocument.Paths[pathInfo];
+
+                foreach (OperationType operation in openPathInfo.Operations.Keys)
+                {
+                    OpenApiOperation opInfo = openPathInfo.Operations[operation];
+                    Console.WriteLine($"{operation} --> {opInfo}");
+                    ParseOperation(operation, opInfo);
+                }
+
+                Console.WriteLine($"{Environment.NewLine}");
+            }
+
+        }
+
+        private static void ParseOperation(OperationType operation, OpenApiOperation opInfo)
+        {
+            Console.WriteLine($"{Environment.NewLine}");
+
+            if(operation == OperationType.Get)
+            {
+                var dynObj = new ExpandoObject();
+                dynObj.AddProperty("What", "Salt");
+
+                foreach(OpenApiParameter parameter in opInfo.Parameters)
+                {
+
+                }
+            }
+
+        }
+    }
+}
