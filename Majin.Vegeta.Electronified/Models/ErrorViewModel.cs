aebbd9df09adfb014d06036fa361cf5b83ac082b
Christos Charalampidis <Chrigeta@hotmail.com>
2019-08-28 18:40:08 +0300

    Added demo of UI Electron.NET application.

---
+++ Majin.Vegeta.Electronified/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Majin.Vegeta.Electronified.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
